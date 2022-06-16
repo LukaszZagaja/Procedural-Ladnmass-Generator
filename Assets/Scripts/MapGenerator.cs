8dcc4334cdc2111b2a2034e0381901858162be3c
Luuq <lukasz2zagaja@gmail.com>
2022-06-16 19:56:49 +0200

    OctavesWorking

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,23 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class MapGenerator : MonoBehaviour
 {
-    public int mapWidth;
-    public int mapHeight;
-    public float noiseScale;
 
-    public int octaves;
-    public float persistance;
-    public float lacunarity;
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
 
-    public bool autoUpdate;
+	public int octaves;
+	[Range(0, 1)]
+	public float persistance;
+	public float lacunarity;
 
-    public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity);
+	public int seed;
+	public Vector2 offset;
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
-    }
+	public bool autoUpdate;
+
+	public void GenerateMap()
+	{
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+		display.DrawNoiseMap(noiseMap);
+	}
+
+	void OnValidate()
+	{
+		if (mapWidth < 1)
+		{
+			mapWidth = 1;
+		}
+		if (mapHeight < 1)
+		{
+			mapHeight = 1;
+		}
+		if (lacunarity < 1)
+		{
+			lacunarity = 1;
+		}
+		if (octaves < 0)
+		{
+			octaves = 0;
+		}
+	}
+
 }
