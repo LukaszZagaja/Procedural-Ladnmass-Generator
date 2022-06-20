15409640f721f043f59c6d4b7c3b4fc05b10bca2
Luuq <lukasz2zagaja@gmail.com>
2022-06-20 19:39:03 +0200

    ColourMapDone

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MapGenerator : MonoBehaviour
 {
 
+	public enum DrawMode {NoiseMap, ColourMap };
+	public DrawMode drawMode;
+
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -24,9 +27,32 @@ public class MapGenerator : MonoBehaviour
 	{
 		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+		Color[] colourMap = new Color[mapWidth * mapHeight];
+		for(int y = 0; y< mapHeight; y++)
+        {
+			for (int x = 0; x < mapWidth; x++)
+            {
+				float currentHeight = noiseMap[x, y];
+				for(int i = 0; i < regions.Length; i++)
+                {
+					if(currentHeight <= regions[i].height)
+                    {
+						colourMap[y * mapWidth + x] = regions[i].colour;
+						break;
+                    }
+                }
+            }
+        }
 
 		MapDisplay display = FindObjectOfType<MapDisplay>();
-		display.DrawNoiseMap(noiseMap);
+		if (drawMode == DrawMode.NoiseMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		}
+		else if (drawMode == DrawMode.ColourMap)
+        {
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
 	}
 
 	void OnValidate()
