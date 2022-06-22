6e09059f1c971ce53737545529c18056f0becd66
Luuq <lukasz2zagaja@gmail.com>
2022-06-22 16:01:18 +0200

    MeshDone

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MapGenerator : MonoBehaviour
 {
 
-	public enum DrawMode {NoiseMap, ColourMap };
+	public enum DrawMode {NoiseMap, ColourMap, Mesh};
 	public DrawMode drawMode;
 
 	public int mapWidth;
@@ -53,6 +53,10 @@ public class MapGenerator : MonoBehaviour
         {
 			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
 		}
+		else if (drawMode == DrawMode.Mesh)
+        {
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
 	}
 
 	void OnValidate()
